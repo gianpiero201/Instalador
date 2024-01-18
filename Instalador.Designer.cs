4444cb40885ffde06d5efb2be28b21e720c94e57
Gian <giglio2001@gmail.com>
2024-01-17 21:55:06 -0300

    init

---
+++ Instalador.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace Instalador
+{
+    partial class Instalador
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Instalador));
+            this.mvvmContext1 = new DevExpress.Utils.MVVM.MVVMContext(this.components);
+            this.layoutControl1 = new DevExpress.XtraLayout.LayoutControl();
+            this.Root = new DevExpress.XtraLayout.LayoutControlGroup();
+            this.emptySpaceItem2 = new DevExpress.XtraLayout.EmptySpaceItem();
+            this.simpleSeparator1 = new DevExpress.XtraLayout.SimpleSeparator();
+            this.LoginHtmlControl = new DevExpress.XtraEditors.HtmlContentControl();
+            this.layoutControlItem1 = new DevExpress.XtraLayout.LayoutControlItem();
+            this.svgImageCollection1 = new DevExpress.Utils.SvgImageCollection(this.components);
+            this.LoginTextEdit = new DevExpress.XtraEditors.Repository.RepositoryItemTextEdit();
+            this.PassTextEdit = new DevExpress.XtraEditors.Repository.RepositoryItemTextEdit();
+            this.LoginBtn = new DevExpress.XtraEditors.Repository.RepositoryItemButtonEdit();
+            ((System.ComponentModel.ISupportInitialize)(this.mvvmContext1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControl1)).BeginInit();
+            this.layoutControl1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Root)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.emptySpaceItem2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.simpleSeparator1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.LoginHtmlControl)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.svgImageCollection1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.LoginTextEdit)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PassTextEdit)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.LoginBtn)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // mvvmContext1
+            // 
+            this.mvvmContext1.ContainerControl = this;
+            // 
+            // layoutControl1
+            // 
+            this.layoutControl1.Controls.Add(this.LoginHtmlControl);
+            this.layoutControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.layoutControl1.Location = new System.Drawing.Point(0, 0);
+            this.layoutControl1.Name = "layoutControl1";
+            this.layoutControl1.OptionsCustomizationForm.DesignTimeCustomizationFormPositionAndSize = new System.Drawing.Rectangle(752, 0, 650, 400);
+            this.layoutControl1.Root = this.Root;
+            this.layoutControl1.Size = new System.Drawing.Size(964, 529);
+            this.layoutControl1.TabIndex = 0;
+            this.layoutControl1.Text = "layoutControl1";
+            // 
+            // Root
+            // 
+            this.Root.EnableIndentsWithoutBorders = DevExpress.Utils.DefaultBoolean.True;
+            this.Root.GroupBordersVisible = false;
+            this.Root.Items.AddRange(new DevExpress.XtraLayout.BaseLayoutItem[] {
+            this.emptySpaceItem2,
+            this.simpleSeparator1,
+            this.layoutControlItem1});
+            this.Root.Name = "Root";
+            this.Root.Padding = new DevExpress.XtraLayout.Utils.Padding(0, 0, 0, 0);
+            this.Root.Size = new System.Drawing.Size(964, 529);
+            this.Root.TextVisible = false;
+            // 
+            // emptySpaceItem2
+            // 
+            this.emptySpaceItem2.AllowHotTrack = false;
+            this.emptySpaceItem2.Location = new System.Drawing.Point(295, 0);
+            this.emptySpaceItem2.Name = "emptySpaceItem2";
+            this.emptySpaceItem2.Size = new System.Drawing.Size(669, 529);
+            this.emptySpaceItem2.TextSize = new System.Drawing.Size(0, 0);
+            // 
+            // simpleSeparator1
+            // 
+            this.simpleSeparator1.AllowHotTrack = false;
+            this.simpleSeparator1.Location = new System.Drawing.Point(294, 0);
+            this.simpleSeparator1.Name = "simpleSeparator1";
+            this.simpleSeparator1.Size = new System.Drawing.Size(1, 529);
+            // 
+            // LoginHtmlControl
+            // 
+            this.LoginHtmlControl.Cursor = System.Windows.Forms.Cursors.Default;
+            this.LoginHtmlControl.HtmlImages = this.svgImageCollection1;
+            this.LoginHtmlControl.HtmlTemplate.Styles = resources.GetString("htmlContentControl1.HtmlTemplate.Styles");
+            this.LoginHtmlControl.HtmlTemplate.Template = resources.GetString("htmlContentControl1.HtmlTemplate.Template");
+            this.LoginHtmlControl.Location = new System.Drawing.Point(2, 2);
+            this.LoginHtmlControl.Name = "LoginHtmlControl";
+            this.LoginHtmlControl.RepositoryItems.AddRange(new DevExpress.XtraEditors.Repository.RepositoryItem[] {
+            this.LoginTextEdit,
+            this.PassTextEdit,
+            this.LoginBtn});
+            this.LoginHtmlControl.Size = new System.Drawing.Size(290, 525);
+            this.LoginHtmlControl.TabIndex = 4;
+            // 
+            // layoutControlItem1
+            // 
+            this.layoutControlItem1.Control = this.LoginHtmlControl;
+            this.layoutControlItem1.Location = new System.Drawing.Point(0, 0);
+            this.layoutControlItem1.Name = "layoutControlItem1";
+            this.layoutControlItem1.Size = new System.Drawing.Size(294, 529);
+            this.layoutControlItem1.TextSize = new System.Drawing.Size(0, 0);
+            this.layoutControlItem1.TextVisible = false;
+            // 
+            // svgImageCollection1
+            // 
+            this.svgImageCollection1.Add("logo", ((DevExpress.Utils.Svg.SvgImage)(resources.GetObject("svgImageCollection1.logo"))));
+            // 
+            // LoginTextEdit
+            // 
+            this.LoginTextEdit.AdvancedModeOptions.Label = "Login";
+            this.LoginTextEdit.Appearance.BorderColor = System.Drawing.Color.Turquoise;
+            this.LoginTextEdit.AutoHeight = false;
+            this.LoginTextEdit.BorderStyle = DevExpress.XtraEditors.Controls.BorderStyles.NoBorder;
+            this.LoginTextEdit.Name = "LoginTextEdit";
+            // 
+            // PassTextEdit
+            // 
+            this.PassTextEdit.AutoHeight = false;
+            this.PassTextEdit.BorderStyle = DevExpress.XtraEditors.Controls.BorderStyles.NoBorder;
+            this.PassTextEdit.Name = "PassTextEdit";
+            this.PassTextEdit.PasswordChar = '*';
+            this.PassTextEdit.UseSystemPasswordChar = true;
+            // 
+            // LoginBtn
+            // 
+            this.LoginBtn.AutoHeight = false;
+            this.LoginBtn.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton()});
+            this.LoginBtn.Name = "LoginBtn";
+            this.LoginBtn.ButtonClick += new DevExpress.XtraEditors.Controls.ButtonPressedEventHandler(this.LoginBtn_ButtonClick);
+            // 
+            // Instalador
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(964, 554);
+            this.Controls.Add(this.layoutControl1);
+            this.Name = "Instalador";
+            this.Padding = new System.Windows.Forms.Padding(0, 0, 0, 25);
+            this.Text = "Instalador";
+            ((System.ComponentModel.ISupportInitialize)(this.mvvmContext1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControl1)).EndInit();
+            this.layoutControl1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.Root)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.emptySpaceItem2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.simpleSeparator1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.LoginHtmlControl)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.svgImageCollection1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.LoginTextEdit)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PassTextEdit)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.LoginBtn)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.Utils.MVVM.MVVMContext mvvmContext1;
+        private DevExpress.XtraLayout.LayoutControl layoutControl1;
+        private DevExpress.XtraLayout.LayoutControlGroup Root;
+        private DevExpress.XtraLayout.EmptySpaceItem emptySpaceItem2;
+        private DevExpress.XtraLayout.SimpleSeparator simpleSeparator1;
+        private DevExpress.XtraEditors.HtmlContentControl LoginHtmlControl;
+        private DevExpress.XtraLayout.LayoutControlItem layoutControlItem1;
+        private DevExpress.Utils.SvgImageCollection svgImageCollection1;
+        private DevExpress.XtraEditors.Repository.RepositoryItemTextEdit LoginTextEdit;
+        private DevExpress.XtraEditors.Repository.RepositoryItemTextEdit PassTextEdit;
+        private DevExpress.XtraEditors.Repository.RepositoryItemButtonEdit LoginBtn;
+    }
+}
---
+++ Instalador.cs
@@ -0,0 +1,31 @@
+﻿using DevExpress.XtraEditors;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Instalador
+{
+    public partial class Instalador : DevExpress.XtraEditors.XtraForm
+    {
+        public Instalador()
+        {
+            InitializeComponent();
+        }
+
+        public void Test()
+        {
+            MessageBox.Show("teste");
+        }
+
+        private void LoginBtn_ButtonClick(object sender, DevExpress.XtraEditors.Controls.ButtonPressedEventArgs e)
+        {
+            MessageBox.Show("teste");
+        }
+    }
+}
